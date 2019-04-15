98018ce5206da8c34616f9226ea938e14aad527d
rotimi-babalola <rotimi.babalola@andela.com>
2019-04-15 19:05:18 +0100

    add routes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,26 @@
 import React, { Fragment } from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import GlobalStyle from '../styles/global';
 import ThemWrapper from '../styles/ThemWrapper';
+
+import routes from './routes';
 
 const App = () => (
   <ThemWrapper>
     <Fragment>
       <GlobalStyle />
-      <h1>Hello world</h1>
+      <Router>
+        <Switch>
+          {routes.default.map(({
+            exact,
+            path,
+            component,
+            id,
+          }) => (<Route key={id} exact={exact} path={path} component={component} />))}
+        </Switch>
+      </Router>
     </Fragment>
   </ThemWrapper>
 );
